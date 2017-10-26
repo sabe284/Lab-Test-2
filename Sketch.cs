98fa4a4ae1072c89e244c66f23fe940c13a94448
sabe284 <30421252+sabe284@users.noreply.github.com>
2017-10-26 19:13:51 +1300

    Tasks 1-5

---
+++ Sketch.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using Pathfinding.Serialization.JsonFx; //make sure you include this using
+
+public class Sketch : MonoBehaviour {
+    //public GameObject myPrefab;
+    public GameObject Cube;
+    public GameObject Sphere;
+    public string _WebsiteURL = "http://chronospatial.azurewebsites.net/tables/Town?zumo-api-version=2.0.0";
+
+    void Start () {
+        //Reguest.GET can be called passing in your ODATA url as a string in the form:
+        //http://{Your Site Name}.azurewebsites.net/tables/{Your Table Name}?zumo-api-version=2.0.0
+        //The response produce is a JSON string
+        string jsonResponse = Request.GET(_WebsiteURL);
+
+        //Just in case something went wrong with the request we check the reponse and exit if there is no response.
+        if (string.IsNullOrEmpty(jsonResponse))
+        {
+            return;
+        }
+
+        //We can now deserialize into an array of objects - in this case the class we created. The deserializer is smart enough to instantiate all the classes and populate the variables based on column name.
+        Town[] towns = JsonReader.Deserialize<Town[]>(jsonResponse);
+
+        //----------------------
+        //YOU WILL NEED TO DECLARE SOME VARIABLES HERE SIMILAR TO THE CREATIVE CODING TUTORIAL
+
+        int i = 0;
+        int totalCubes = towns.Length;
+        //float totalDistance = 2.9f;
+        //----------------------
+
+        //We can now loop through the array of objects and access each object individually
+        foreach (Town town in towns)
+        {
+            //Example of how to use the object
+            Debug.Log("This towns name is: " + town.TownName);
+            //----------------------
+            //YOUR CODE TO INSTANTIATE NEW PREFABS GOES HERE
+            float perc = i / (float)totalCubes;
+            float sin = Mathf.Sin(perc * Mathf.PI / 2);
+
+            float x = town.Longitude;
+            float y = town.Latitude;
+            float z = town.Altitude;
+            
+            string symbol = town.Symbol;
+
+            if (symbol == "Cube")
+            {
+                var newCube = (GameObject)Instantiate(Cube, new Vector3(x, y, z), Quaternion.identity);
+
+                newCube.GetComponent<CubeScript>().SetSize(town.Size);
+                //(.45f * (1.0f - perc));
+                //newCube.GetComponent<CubeScript>().rotateSpeed = .2f + perc * 4.0f;
+                newCube.transform.Find("New Text").GetComponent<TextMesh>().text = town.TownName;
+
+
+            }
+
+            if (symbol == "Sphere")
+            {
+                var newSphere = (GameObject)Instantiate(Sphere, new Vector3(x, y, z), Quaternion.identity);
+
+                newSphere.GetComponent<SphereScript>().SetSize(town.Size);
+                newSphere.transform.Find("New Text").GetComponent<TextMesh>().text = town.TownName;
+            }
+          
+            i++;
+
+            //----------------------
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
